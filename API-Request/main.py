1fbb5d9ad7222614de376de2fed6faff456da79b
NekoNeko <20726844+PatrickSantoZZ@users.noreply.github.com>
2025-12-15 10:53:48 +0100

    [api-request] filter by user

--- API-Request/main.py
+++ API-Request/main.py
@@ -15,6 +15,9 @@ def request_api():
     else:
         print(f"Error: {response.status_code}")
 
+def filter_by_user(data, user_id):
+    return [item for item in data if item['userId'] == user_id]
+
 def save_to_csv(data, filename):
     with open(filename, 'w', newline='') as file:
         writer = csv.writer(file)
@@ -25,5 +28,11 @@ def save_to_csv(data, filename):
 if __name__ == "__main__":
     data = request_api()
     if data:
+        # small addition to filter by user id
+        #filtered_data = filter_by_user(data, 1)
+        #save_to_csv(filtered_data, 'filtered_posts.csv')
+        #print("Filtered data saved to filtered_posts.csv")
         save_to_csv(data, 'posts.csv')
         print("Data saved to posts.csv")
+    else:
+        print("No data received from the API/Function.")
